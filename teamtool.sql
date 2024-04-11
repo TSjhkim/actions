f8301caa2b02c4f63c8f8a5c9cbd80fb9ad12f69
김지훈 <jkjh1717@naver.com>
2024-04-11 17:45:28 +0900

    add 545 teamtool.sql

--- teamtool.sql
+++ teamtool.sql
@@ -5,4 +5,4 @@ CREATE TABLE Teamtool (
 );
 
 INSERT INTO Teamtool (Product, Version, Users)
-VALUES ('Space', 2024, 100), ('CodeCanvas', 2024, 200), ('IDE Provisioner', 2024, 300);
+VALUES ('Space', 2024, 100), ('CodeCanvas', 2024, 200), ('IDE Provisioner', 2024, 300), ('AI Enterprise', 2024, 500);
