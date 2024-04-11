ac1fe3f0669b4aa71b7d58eb4b9ff7c8e7099631
김지훈 <jkjh1717@naver.com>
2024-04-11 18:16:05 +0900

    615

--- person.sql
+++ person.sql
@@ -5,4 +5,4 @@ CREATE TABLE Person (
 );
 
 INSERT INTO Person (Name, Age, Height)
-VALUES ('Kim', 20, 182), ('Lee', 21, 183), ('Jin', 22, 184), ('Hong', 23, 185), ('Kang', 24, 186), ('Jung', 25, 187);
+VALUES ('Kim', 20, 182), ('Lee', 21, 183), ('Jin', 22, 184);
--- teamtool.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Teamtool (
-    Product NVARCHAR(40),
-    Version INT,
-    Users INT
-);
-
-INSERT INTO Teamtool (Product, Version, Users)
-VALUES ('Space', 2024, 100), ('CodeCanvas', 2024, 200), ('IDE Provisioner', 2024, 300);
