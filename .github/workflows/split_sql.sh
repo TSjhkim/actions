59c1a3957453234784c46142572df08f8b5f6e29
TSjhkim <120078769+TSjhkim@users.noreply.github.com>
2024-04-12 11:29:46 +0900

    Update split_sql.sh

--- .github/workflows/split_sql.sh
+++ .github/workflows/split_sql.sh
@@ -1,16 +1,9 @@
 git config --global user.email "jhkim0228@tangunsoft.com"
 git config --global user.name "TSjhkim"
 
-ssh-keygen -t rsa -b 4096 -C "jhkim0228@tangunsoft.com"
-git remote set-url origin git@github.com:TSjhkim/actions1.git
-
 git clone https://github.com/TSjhkim/actions1.git
 cd actions1
 
 find . -type f -name "*.sql" | while IFS= read -r file; do
-    awk -v RS=';' '{print > ("tmp_" NR ".sql")}' "$file"
-    git add tmp_*.sql
-    git commit -m "Add split SQL files from $file"
+    awk -v RS=';' '{print > ("~/tmp_" NR ".sql")}' "$file"
 done
-
-git push git@github.com:TSjhkim/actions1.git master
