16bd5e84006b22ac386de05903dbda24a6b98b70
TSjhkim <120078769+TSjhkim@users.noreply.github.com>
2024-04-12 11:52:51 +0900

    Update split_sql.sh

--- .github/workflows/split_sql.sh
+++ .github/workflows/split_sql.sh
@@ -4,6 +4,7 @@ git config --global user.name "TSjhkim"
 git clone https://github.com/TSjhkim/actions1.git
 cd actions1
 
+export SQL_FILES_PATH=$(pwd)
 find . -type f -name "*.sql" | while IFS= read -r file; do
     awk -v RS=';' '{print > ("./tmp_" NR ".sql")}' "$file"
 done
